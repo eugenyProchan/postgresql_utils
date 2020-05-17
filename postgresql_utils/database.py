ef8c615816c5c5982bd689e5cfe7f0f19ce0b5c9
eugenyProchan <pro4anjey@gmail.com>
2020-05-17 11:48:30 +0500

    change indent level for execute and copy to db

--- postgresql_utils/database.py
+++ postgresql_utils/database.py
@@ -20,43 +20,43 @@ class DataBasePostgres:
         self._dns = dns
         self._column_type = self._get_column_type()
 
-        def execute(self,
-                    query: str,
-                    cursor_factory: Optional = None)-> Union[dict, list, tuple, None]:
-            with self._executed_cursor(query, cursor_factory) as cursor:
-                message_from_db = cursor.statusmessage
-                if message_from_db in ['CREATE TABLE',
-                                       'TRUNCATE TABLE',
-                                       'DELETE TABLE',
-                                       'REFRESH MATERIALIZED VIEW']:
-                    return None
-                return cursor.fetchall()
+    def execute(self,
+                query: str,
+                cursor_factory: Optional = None)-> Union[dict, list, tuple, None]:
+        with self._executed_cursor(query, cursor_factory) as cursor:
+            message_from_db = cursor.statusmessage
+            if message_from_db in ['CREATE TABLE',
+                                   'TRUNCATE TABLE',
+                                   'DELETE TABLE',
+                                   'REFRESH MATERIALIZED VIEW']:
+                return None
+            return cursor.fetchall()
 
-        def copy_to_db(self, query: str,
-                            target_db: 'DataBasePostgres',
-                            target_table_name: str,
-                            chank_size:Optional[int] = 2000) -> None:
+    def copy_to_db(self, query: str,
+                        target_db: 'DataBasePostgres',
+                        target_table_name: str,
+                        chank_size:Optional[int] = 2000) -> None:
 
-        with self._executed_cursor(query: str) as source_cursor, target_db._get_cursor() as target_cursor:
-            create_sql = self._generate_create_table_sql_from_cursor(source_cursor, target_table_name)
-            insert_sql = self._generate_insert_table_sql_from_cursor(source_cursor, target_table_name)
+    with self._executed_cursor(query: str) as source_cursor, target_db._get_cursor() as target_cursor:
+        create_sql = self._generate_create_table_sql_from_cursor(source_cursor, target_table_name)
+        insert_sql = self._generate_insert_table_sql_from_cursor(source_cursor, target_table_name)
 
-            target_db.execute(create_sql)
-            target_db.execute(f'truncate table {target_table_name}')
+        target_db.execute(create_sql)
+        target_db.execute(f'truncate table {target_table_name}')
 
-            try:
-                while True:
-                    records = source_cursor.fetchmany(chank_size)
-                    if not records:
-                        break
-                    execute_values(target_cursor,
-                                   insert_sql,
-                                   records)
-                target_cursor.connection.commit()
-                logger.info(f'Data have been transfered in {target_table_name}')
+        try:
+            while True:
+                records = source_cursor.fetchmany(chank_size)
+                if not records:
+                    break
+                execute_values(target_cursor,
+                                insert_sql,
+                                records)
+            target_cursor.connection.commit()
+            logger.info(f'Data have been transfered in {target_table_name}')
 
-            except DatabaseError as exc:
-                raise exc('Exception in copy_to_db method ' + query)
+        except DatabaseError as exc:
+            raise exc('Exception in copy_to_db method ' + query)
 
     def _get_cursor(self, cursor_factory:Optional=None) -> psycopg2.extensions.cursor:
         try:
